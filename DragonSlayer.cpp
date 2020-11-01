20b2d40302c7c8ab889747ac5aecb2f2de76b877
MasonSelf <masonselfmusic@gmail.com>
2020-11-01 15:17:04 +0000

    utility.cpp errors

--- DragonSlayer.cpp
+++ DragonSlayer.cpp
@@ -1,33 +1,48 @@
 #include "DragonSlayer.h"
 #include "Dragon.h"
 #include "Utility.h"
+#include "AttackItem.h"
 
-DragonSlayer::DragonSlayer( std:: string name_, int hitPoints, int armor ) : Character(hitPoints, armor, 57 ), name(name_) {}
+DragonSlayer::DragonSlayer( std:: string name_, int hitPoints, int armor ) : Character(hitPoints, armor, 57 ), name(name_) 
+{
+    
+}
 
 const std::string& DragonSlayer::getName()
 {
     return name;
 }
 
-// void DragonSlayer::attack(Character& other)
-// {
-//     std::cout << name << " is attacking " << other.getName() << " !!" << std::endl;
-//     if( auto* dragon = dynamic_cast<Dragon*>(&other) )
-//     {
-//         assert(false);
-//         //DragonSlayers get a 10x boost when attacking dragons, from their attack item.
-//         //so they should USE their attack item before attacking the dragon... 
-//         //note that items are single-use only, so you need to reset it after use.  
-//         //look in the Character class for how the other item types are reset after use.
-//         while( dragon->getHP() > 0 )
-//         {
-//           dragon->takeDamage(attackDamage);
-//         }
-//     }
+void DragonSlayer::attack(Character& other)
+{
+    std::cout << name << " is attacking " << other.getName() << " !!" << std::endl;
+    if( auto* dragon = dynamic_cast<Dragon*>(&other) )
+    {
+        //assert(false);
+        //DragonSlayers get a 10x boost when attacking dragons, from their attack item.
+        //so they should USE their attack item before attacking the dragon... 
+        //note that items are single-use only, so you need to reset it after use.  
+        //look in the Character class for how the other item types are reset after use.
+
+        if (attackItem == nullptr)
+        {
+
+        }
+        else 
+        {
+            attackItem->use(this);
+            attackItem.reset();
+        }
+
+        while( dragon->getHP() > 0 )
+        {
+          dragon->takeDamage(attackDamage);
+        }
+    }
         
-//     Character::attack(other);
+    Character::attack(other);
         
-// }
+}
 
 std::string DragonSlayer::getStats()
 {
